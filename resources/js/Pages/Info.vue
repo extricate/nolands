545072dd58fe5019b92504a25fa0b3167b79f3a3
Herman <extricate@users.noreply.github.com>
2022-04-27 10:46:35 +0200

    Update info.vue

--- resources/js/Pages/Info.vue
+++ resources/js/Pages/Info.vue
@@ -8,91 +8,24 @@
                     <div class="overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 pt-12 text-white">
                             <p class="font-serif">Lieve Nolanders,&nbsp;</p>
-                            <p class="text-sm text-gray-200 pb-3">Laatste update 24/11/2021</p>
-                            <p>Ondanks de cancelcultuur, aangeslingerd door Mark en Hugo, staat editie 3 van Nolands op
-                                de planning voor het pinksterweekend van 2022. Dat is van 3 tot 6 juni, waarbij team
-                                opbouw op 2 juni wordt verwacht. We gaan weer naar dezelfde locatie en veel zal
-                                hetzelfde zijn als afgelopen jaar, maar dan met een aantal upgrades! Het grootste
-                                verschil ten opzichte van vorig jaar is dat we dit jaar hopelijk geen mensen hoeven te
-                                weigeren: iedereen die wil mag komen als tante C een beetje meewerkt. &nbsp;</p>
-                            <p>Daarnaast is de organisatie wat uitgebreid om deze hordes mensen te kunnen managen. Wie
-                                helpen er mee en pakt welke rol:</p>
-                            <p><br></p>
-                            <p>Jolijn &amp; Jelle: Duo loco locatie&nbsp;</p>
-                            <p>Michiel: Mr. Muscle Cleancommandant &nbsp;</p>
-                            <p>Thomas: EntertainThom</p>
-                            <p>Herman: Herrie in de keuken (en aan de bar)</p>
-                            <p>Noah: Chef overzicht, geld en doorbitch&nbsp;</p>
-                            <p><br></p>
-                            <p>Informatie zal dit jaar gedeeld worden via onze gloednieuwe website (!!!) en via een
-                                &lsquo;announcements&rsquo; groepsapp waar alleen wij iets kunnen sturen.&nbsp;</p>
-                            <p><br></p>
-                            <p>Wie?</p>
-                            <p>Iedereen die mee wilt! Heb je vrienden die mee willen, dan kunnen ze zich ook aanmelden.
-                                Deadline voor het inschrijven is 2 januari 2022, daarna gaan we begroten en kijken of,
-                                en zo ja, hoeveel plek er nog is. Wees er dus snel bij&hellip;&nbsp;</p>
-                            <p><br></p>
-                            <p>Waar?</p>
-                            <p>Kampeerboerderij Fleuren in De Mortel, zelfde locatie als vorig jaar. Bij de geweldig
-                                lieve Frans Fleuren! Voor de nieuwelingen: De eigenaar, ons Frans, verhuurt zijn
-                                boerderij voornamelijk aan groepen jongeren. De locatie is hufterproof en hij is wat
-                                geluid gewend. De ideale locatie voor dit weekend, dat hebben we vorig jaar gemerkt.&nbsp;</p>
-                            <p><br></p>
-                            <p>Wat?</p>
-                            <p>Het idee is hetzelfde als vorig jaar: maar ditmaal 3.0! Wederom met geluidsinstallatie,
-                                DJ&rsquo;s, aankleding, lichten, activiteiten, biertap, eten, ALLES. En daarnaast
-                                geweldige, creatieve teams waar je je voor kunt aanmelden. Hebben jullie direct
-                                inspiratie? Misschien is er wel ruimte voor jouw eigen idee! Stuur degene onder wie jouw
-                                idee valt (zie lijst namen hierboven) een appje en we voegen het toe aan de idee&euml;npot.
-                                Na 2 januari wordt het mogelijk om je aan te melden voor een bestaand team. Het is wel
-                                de bedoeling dat je ook echt wat bijdraagt aan het weekend: mocht je jezelf niet
-                                inschrijven voor een team schrijven wij jou in (Mr. Muscle ontvangt jou graag)
-                                😉.&nbsp;</p>
-                            <p>En dat alles voor een schamele 110 euro! Wij nemen de inflatie voor onze
-                                rekening.&nbsp;</p>
-                            <p><br></p>
-                            <p>Wat zit hierbij:</p>
-                            <p>-<span style="white-space:pre;">&nbsp; &nbsp;&nbsp;</span>Locatie, inclusief TE GEKKE
-                                aankleding</p>
-                            <p>-<span style="white-space:pre;">&nbsp; &nbsp;&nbsp;</span>Geluidsinstallatie. Dit jaar
-                                iets minder hard om Fransie ook wat slaap te gunnen.&nbsp;</p>
-                            <p>-<span style="white-space:pre;">&nbsp; &nbsp;&nbsp;</span>Onbeperkt bier (HAHA BIER)</p>
-                            <p>-<span style="white-space:pre;">&nbsp; &nbsp;&nbsp;</span>3x avondeten, maar afhankelijk
-                                van het budget ook lunch en ontbijt. Horen jullie later!&nbsp;</p>
-                            <p>-<span style="white-space:pre;">&nbsp; &nbsp;&nbsp;</span>Merchandise, awesome</p>
-                            <p>-<span style="white-space:pre;">&nbsp; &nbsp;&nbsp;</span>Ultiem vermaak en fenomenale
-                                activiteiten</p>
-                            <p><br></p>
-                            <p>Wat zit hier niet bij?</p>
-                            <p>-<span style="white-space:pre;">&nbsp; &nbsp;&nbsp;</span>Sterke drank</p>
-                            <p>-<span style="white-space:pre;">&nbsp; &nbsp;&nbsp;</span>Snacks, en misschien dus geen
-                                ontbijt/lunch (maar who needs that als je onbeperkt bier hebt)</p>
-                            <p>-<span style="white-space:pre;">&nbsp; &nbsp;&nbsp;</span>Andere luxe versnaperingen</p>
-                            <p>Al het overgebleven geld wordt na Nolands aan een goed doel gedoneerd. Of we investeren
-                                in Bitcoin. We kijken nog wel even.&nbsp;</p>
-                            <p><br></p>
-                            <p>Worst kaas scenario&nbsp;</p>
-                            <p>Als je tijdig aangeeft dat je toch niet mee kan (uiterlijk 2 maanden voor Nolands) krijg
-                                je het geld gerestitueerd. Als het op een later moment komt, bekijken we het samen even
-                                &ndash; dan stuur je Noah even een appje.&nbsp;</p>
-                            <p>Stel, in het ergste geval, Nolands gaat niet door (door bijvoorbeeld overmacht), dan
-                                krijgt iedereen zo veel mogelijk zijn geld terug minus de kosten die gemaakt zijn. Maar
-                                daar gaan we natuurlijk niet vanuit! De afgelopen 2 jaar is het ons ook gelukt, dus we
-                                gaan er weer voor dit jaar.&nbsp;</p>
-                            <p><br></p>
-                            <p>Hoe meld ik me aan?</p>
-                            <p>Via onze speciale website! Hierop zullen we ook nieuwtjes, feitjes, FAQtjes plaatsen
-                                wanneer wij daar zin in hebben. Meld je aan voor 2 januari 2021! Daarna inventariseren
-                                we hoeveel plek we nog hebben. Het is dit jaar niet mogelijk om &lsquo;halve&rsquo;
-                                kaarten te kopen.&nbsp;</p>
-                            <p>-<span style="white-space:pre;">&nbsp; &nbsp;&nbsp;</span>Ga naar de volgende website:
-                                Nolands.live</p>
-                            <p>-<span style="white-space:pre;">&nbsp; &nbsp;&nbsp;</span>Cre&euml;er een account</p>
-                            <p>-<span style="white-space:pre;">&nbsp; &nbsp;&nbsp;</span>Betaal de tikkie voor 2
-                                januari!&nbsp;</p>
-                            <p>WIJ HEBBEN ER HEEL VEEL ZIN IN!&nbsp;</p>
-                            <p><br></p>
-                            <p>Joe, team Orgi</p>
+                            <p class="text-sm text-gray-200 pb-3">Laatste update 26/04/2022</p>
+                            <p>Wie? 109 Nolanders die allemaal hun steentje bijdragen.</p>
+
+<p>Waar en wanneer? Op 3 juni is iedereen welkom vanaf 10 uur bij Kampeerboerderij Fleuren op de Zwarte Waterweg 9, 5425 RM De Mortel. Team Opbouw en Team Aankleding wordt op donderdag 2 juni begin van de middag verwacht. Mocht je al op donderdag willen komen, contact dan Noah even.</p>
+
+<p>Vervoer De Mortel is zeer slecht bereikbaar met het openbaar vervoer. Op locatie zijn genoeg parkeerplekken. Mocht je zelf nou geen vervoer hebben, raden wij je aan om via de groepschat te carpoolen! Als dit niet lukt, neem dan even contact op met Noah, dan kijkt hij met jou verder.</p>
+
+<p>Elektriciteit Op de camping zelf is geen stroom. Bij de boerderij, veranda en de feestschuur zijn er wel stopcontacten. Hier kan je, mits ze niet gebruikt worden, op eigen risico gebruik van maken om bijvoorbeeld je telefoon op te laden.</p>
+
+<p>(Geluids)overlast We willen natuurlijk geen overlast veroorzaken, hier hebben we een aantal maatregelen voor genomen: - We zullen dit jaar een kleinere soundsystem huren t.o.v. vorige jaren. - Op het moment dat het rolluik gesloten is willen we iedereen verzoeken de deur naar de feestschuur dicht te houden!! - In de avond is er geen versterkte muziek buiten en geen overmatig geschreeuw. - Op het moment dat we toch overlast veroorzaken, schakelen we in de avonden over naar silent disco. - Gooi afval in de prullenbakken en laat het niet slingeren</p>
+
+<p>Eten Elke dag is er avondeten geregeld. De exacte tijdstippen zijn nog niet bekend, voor de vrijdag (aankomstdag) is het eten van 18:00 tot 19:30. Zorg er dus voor dat je er op tijd bent voor het avondeten! Mocht je het niet redden, zelfs Thuisbezorgd is in De Mortel. Op zaterdag en zondag wordt er eten gemaakt door team Eten.</p>
+
+<p>Drinken In de schuur staat een tap waar bier uit komt, die is te gebruiken voor iedereen die de keuze heeft doorgegeven om bier te drinken. Mocht een fust leeg zijn, mag je deze vervangen of even doorgeven aan iemand die dit kan. Het drinken van de overige Nolanders (wijn en fris) staat in de koelkast naast de tap.</p>
+
+<p>Paklijst Volgt z.s.m.</p>
+
+<p>Corona Het is een woord dat nu niet meer zo relevant is, maar we gaan er als organisatie vanuit dat iedereen verantwoordelijk omgaat met eventuele klachten.</p>
                         </div>
                     </div>
                 </div>
